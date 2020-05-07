6e5b70db5ced12c9b3b197d31edc2fcbde7d2932
Jose Reyes <jaeyow@gmail.com>
2020-05-07 20:09:29 +1000

    set playground endpoint

--- graphql.js
+++ graphql.js
@@ -2,7 +2,7 @@ const { ApolloServer } = require('apollo-server-lambda');
 const typeDefs = require('./typedefs');
 const resolvers = require('./resolvers');
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, playground: { endpoint: "/dev/graphql" }});
 
 // to run on your AWS Lambda (testing)
 exports.graphqlHandler = server.createHandler({
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const typeDefs = require('./typedefs');
 const resolvers = require('./resolvers');
 
 // to run locally or on your EC2 instance
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers , playground: { endpoint: "/dev/graphql" }});
 
 server.listen().then(({ url }) => {
     console.log(`🚀  F1 GraphQL Server ready at ${url}`);
